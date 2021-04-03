34deaba543eb63f9c7f4d2964eabad0e5c70efa8
koutselakismanos <koutselakismanos@gmail.com>
2021-04-03 17:34:57 +0300

    background.js

--- background.js
+++ background.js
@@ -1,32 +1,29 @@
-chrome.runtime.onInstalled.addListener(async () => {
-    chrome.contextMenus.create({"title": "Wordreference", "id": "parent", contexts: ['selection']});
+const menus = {
+    PARENT: 'PARENT',
+    TRANSLATE: 'TRANSLATE',
+}
 
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({'title': 'Go to WordReference', 'id': menus.PARENT, contexts: ['selection']});
+    chrome.contextMenus.create({'title': 'Translate', 'id': menus.TRANSLATE, contexts: ['selection']});
 });
 
-chrome.contextMenus.onClicked.addListener((event) => {
-    if (event != null) {
-        goToWordReference(event.selectionText)
+chrome.contextMenus.onClicked.addListener(event => {
+    const {menuItemId, selectionText} = event;
+    switch (menuItemId) {
+        case menus.PARENT:
+            goToWordReference(selectionText)
+            break;
+        case menus.TRANSLATE:
+            // TODO
+            break;
+        default:
+            console.error('No matching menu id.')
     }
 })
-
-const translate = async (word, from = 'en', to = 'gr') => {
-    const url = `http://www.wordreference.com/${from}${to}/${word}`
-    const html = await request({
-        method: 'GET',
-        uri: url,
-        headers: {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-        }
-    })
-    return processHtml(html)
-};
 
 const goToWordReference = (word, from = 'en', to = 'gr') => {
     const url = `http://www.wordreference.com/${from}${to}/${word}`
 
     chrome.tabs.create({url})
 }
-
-const processHtml = (html) => {
-    // :D
-};
